90ed8bf1dfa74bfc86e609cb415f788d086ec4ea
Dhurba Baral <dhurbabaralam@gmail.com>
2022-01-28 10:28:02 +0545

    get, put and delete routes for request

--- src/request/request.js
+++ src/request/request.js
@@ -37,4 +37,57 @@ router.post('/request',(req,res)=>{
     })
 });
 
+
+//get all requests
+router.get('/request',(req,res)=>{
+    const sql="SELECT * FROM request";
+    db.query(sql,(err,result)=>{
+        if(err){
+            res.status(500).send(err);
+        }
+        res.status(200).send(result);
+    })
+});
+
+//get a request by id
+router.get('/request/:id',(req,res)=>{
+    const sql="SELECT * FROM request WHERE id=?";
+    db.query(sql,[req.params.id],(err,result)=>{
+        if(err){
+            res.status(500).send(err);
+        }
+        res.status(200).send(result);
+    })
+});
+
+//update a request by id
+router.put('/request/:id', (req,res)=>{
+    const sql="UPDATE request SET name=?,address=?,contact=?,email=?,bloodGroup=?,description=? WHERE id=?";
+    db.query(sql,[
+        req.body.name,
+        req.body.address,
+        req.body.contact,
+        req.body.email,
+        req.body.bloodGroup,
+        req.body.description,
+        req.params.id
+    ],(err,result)=>{
+        if(err){
+            res.status(500).send(err);
+        }
+        res.status(200).send(result);
+    })
+});
+
+//delete a request by id
+router.delete('/request/:id',(req,res)=>{
+    const sql="DELETE FROM request WHERE id=?";
+    db.query(sql,[req.params.id],(err,result)=>{
+        if(err){
+            res.status(500).send(err);
+        }
+        res.status(200).send(result);
+    })
+});
+
 module.exports=router;
